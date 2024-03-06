dd44d5c74b3fe725e78089f07cca0b881d10b27c
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-06 12:10:29 -0600

    Add files via upload

---
+++ 003_Concatenacion_V1_20310430.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python
+# _#_ coding: cp1252 _*_
+# _#_ coding: 850 _*_
+# _#_ coding: utf-8 _*_
+
+
+PrimeraPalabra="Programacion"
+SegundaPalabra="Sencilla"
+
+Palabra1y2=PrimeraPalabra+" "+" "+SegundaPalabra+"."
+print(Palabra1y2)
