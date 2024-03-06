1598b2a31045993255267d17a14dfb7b0f61895b
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-06 12:20:29 -0600

    Add files via upload

---
+++ 004_Metodo_upper_lowe_title_V1_20310430.py
@@ -0,0 +1,6 @@
+nombre= "jose luis gomez contreras"  #Funcion title: Pone mayuscula en la primera letra de cada palabra
+nombre=nombre.title()
+print(nombre)
+
+print(nombre.upper())
+print(nombre.lower())
