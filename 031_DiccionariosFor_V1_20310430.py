93ee3cd15d9c86a023a217ccad4a08d6b7011e31
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-06 17:18:03 -0600

    Add files via upload

---
+++ 031_DiccionariosFor_V1_20310430.py
@@ -0,0 +1,17 @@
+
+
+teclado1 = {
+    "Categoria": "Teclados",
+    "Modelo": "Hyperx Allow FPS Pro",
+    "Precio": "89,99"
+    }
+
+
+teclado2 = {
+    "Categoria": "Teclados",
+    "Modelo": "Corsair K55 RGB",
+    "Precio": "59,99"  
+    }
+
+for x, y in teclado2.items():
+    print(x,": ",y)
---
+++ 032_,MetodosDiccionarios_V1_20310430.py
@@ -0,0 +1,16 @@
+
+teclados={
+teclado1 = {
+    "Categoria": "Teclados",
+    "Modelo": "Hyperx Allow FPS Pro",
+    "Precio": "89,99"
+    }
+
+
+teclado2 = {
+    "Categoria": "Teclados",
+    "Modelo": "Corsair K55 RGB",
+    "Precio": "59,99"  
+    }
+}
+print(len(teclados))
