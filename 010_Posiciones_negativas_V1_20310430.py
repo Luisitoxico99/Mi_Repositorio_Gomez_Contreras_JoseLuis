f6b1f3c820994824d5a5827d41ed289307a8449b
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-06 13:20:48 -0600

    Add files via upload

---
+++ 010_Posiciones_negativas_V1_20310430.py
@@ -0,0 +1,3 @@
+paises=["Mexico","E.U.","Canada","Brasil","Argentina","Bolivia","Colombia","Panama","Costa rica","Chile","El salvador","Honduras","Haiti","Alaska","Guatemala","Peru" ]
+print("El ultimo pais es:" + paises[-1])
+print("El anterior al penultimo es:" + paises[-3])
