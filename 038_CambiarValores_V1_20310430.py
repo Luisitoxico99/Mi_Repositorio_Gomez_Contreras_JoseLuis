f1bd9a713b1b387f726423adc792e6d8947eb2a3
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-07 20:33:09 -0600

    Update 038_CambiarValores_V1_20310430.py

--- 038_CambiarValores_V1_20310430.py
+++ 038_CambiarValores_V1_20310430.py
@@ -1,14 +1,14 @@
 
-class usuarios:
+class usuarios: #Se crea la clase
     def __init__(usuario, nombre, edad):
-        usuario.nombre = nombre
-        usuario.edad = edad
+        usuario.nombre = nombre      #Las variables son:
+        usuario.edad = edad                        
         
     def muestra_datos(datos):
-        print("El nombre del usuario es: " + datos.nombre, datos.edad)
+        print("El nombre del usuario es: " + datos.nombre, datos.edad) #Se hace print del texto y las variables 
         
-usuario1 = usuarios("Juancho", 76)
-usuario1.muestra_datos()
+usuario1 = usuarios("Juancho", 76)    #Datos de usuario
+usuario1.muestra_datos()                
 
-usuario1.edad = 65
-usuario1.muestra_datos()
+usuario1.edad = 65                    #Declaramos variable
+usuario1.muestra_datos()              #Se muestran los valores
