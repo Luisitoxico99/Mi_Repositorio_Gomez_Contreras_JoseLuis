56a56955b4860687e4b74f7f6013dc814f82597f
Luisitoxico99 <162272470+Luisitoxico99@users.noreply.github.com>
2024-03-07 20:37:13 -0600

    Update 037_Init_V1_20310430.py

--- 037_Init_V1_20310430.py
+++ 037_Init_V1_20310430.py
@@ -1,20 +1,20 @@
 
-class usuarios():
+class usuarios():    #Creacion de clase
     
     def __init__(self,nombre,pin):
-        self.nombre=nombre
+        self.nombre=nombre  #Variaboles de la clase
         self.pin=pin
         
     def saludo(self):
-        print("Saludos" + self.nombre + "Sesion iniciada correctamente")
+        print("Saludos" + self.nombre + "Sesion iniciada correctamente")  #Se imprimen los textos junto con el nombre
     
     def despedida(self):
-        print(self.nombre + "Se ha cerrado sesion")
+        print(self.nombre + "Se ha cerrado sesion") #Se imprime el nombre y los textos del estado de la sesion
         
-usuario1 = usuarios ("Luis","1234")
+usuario1 = usuarios ("Luis","1234")  #Datos de variables
 
-usuario2 = usuarios ("Julieta","3546")
+usuario2 = usuarios ("Julieta","3546") #Datos de usuario2
 
-usuario1.saludo()
+usuario1.saludo()  #Usuarios y acciones
 usuario2.saludo()
-usuario1.despedida()
+usuario1.despedida()  
